f312d8567e7ca59ce7ff54847d1dd0bbe037cb90
Pradipa Y <merakayamsstraat@gmail.com>
2025-03-21 16:16:19 +0800

    deklarasi koneksi ke sql server

--- CRUD_Database_PABD/Form1.cs
+++ CRUD_Database_PABD/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,27 @@ namespace CRUD_Database_PABD
 {
     public partial class Form1: Form
     {
+
+        private string connectionString = "Data Source=LAPTOP-Q7EVPB6K\\PRADIPAYOGANANDA;Initial Catalog=OrganisasiMahasiswa;Integrated Security=True";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
